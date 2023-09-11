2632f546fc85231d0f65ed46c40bd4bab327fbfe
Adil Wadud <adilwadud10@gmail.com>
2023-09-11 12:06:44 +0100

    ready to use api's

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 const fruitForm = document.querySelector("#inputSection form");
 
-console.log(fruitForm);
+const fruitList = document.querySelector("#fruitSection ul");
+
+// const addFruit = fruit => {
+//     const li = document.createElement("li");
+//     li.textContent = fruit;
+//     fruitList.appendChild(li);
+// }
+
+function addFruit(fruit) {
+    if (fruit) {
+        const li = document.createElement("li");
+        li.textContent = fruit;
+        fruitList.appendChild(li);
+    }
+}
+
+
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault();
+    addFruit(e.target.fruitInput.value);
+    e.target.fruitInput.value = "";
+
+
+});
+
+
+fruitList.addEventListener("click", e => {
+    fruitList.removeChild(e.target);
+});
+
 
